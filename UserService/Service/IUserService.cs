3e9a9c6c6d4237f80902cecf5698a79737859bc7
niccle27 <niccle27@gmail.com>
2018-10-22 23:39:06 +0200

    userService

---
+++ UserService/Service/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace UserService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IUserService
+    {
+        #region serviceFunctions
+        [OperationContract]
+        string Connect(string userLogin, String userPassword);
+        #endregion
+
+    }
+}
