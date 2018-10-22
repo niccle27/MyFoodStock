925c1eafdf20ce734b6e3ad44748a48419a5db30
niccle27 <niccle27@gmail.com>
2018-10-22 16:20:18 +0200

    Initial commit

---
+++ AuthenticationService/AuthenticationService.svc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AuthenticationService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AuthenticationService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AuthenticationService.svc or AuthenticationService.svc.cs at the Solution Explorer and start debugging.
+    public class AuthenticationService : IAuthenticationService
+    {
+        #region ServiceMethod
+        public string toLogin(string userLogin, string userPassword)
+        {
+            if (GetHashPassword(userPassword) == GetHashPassword("coucou"))//TODO replace right part by database request
+            {
+                return GenerateToken(userLogin);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion
+
+        #region privateMethod
+
+
+
+        #endregion
+
+        #region privateStaticFunction
+
+        static String GetHashPassword(String password)
+        {
+            Byte[] output;
+            using (SHA512 hashingUnit = new SHA512Managed())
+            {
+                output= hashingUnit.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < output.Length; i++)
+            {
+                sb.Append(output[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+        static String GenerateToken(String username)
+        {
+            Byte[] output;
+            using (MD5 hashingUnit = MD5.Create())
+            {
+                Random rand = new Random();
+                output=hashingUnit.ComputeHash(Encoding.UTF8.GetBytes(username + rand.Next(99999).ToString()));
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < output.Length; i++)
+            {
+                sb.Append(output[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        #endregion
+
+    }
+}
